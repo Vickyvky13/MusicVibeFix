b92e7563ac41117ce00cc064f08e83614a424bd4
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-08-23 21:26:08 +0530

    Update vc.py

--- InflexMusic/plugins/tools/vc.py
+++ InflexMusic/plugins/tools/vc.py
@@ -7,17 +7,12 @@ from InflexMusic import app
 from InflexMusic.misc import SUDOERS 
 from InflexMusic.utils.database import ( 
     get_active_chats, 
-    get_active_video_chats, 
+    get_active_video_chats,
+    get_served_chats,
+    get_served_users
 ) 
 
-random_photos = [ 
-    "https://telegra.ph/file/06e0e6ab4c325746bd496.jpg", 
-    "https://telegra.ph/file/ee4b07cdda3bdd2b6537c.jpg", 
-    "https://telegra.ph/file/f3f55cad57397cd06623e.jpg", 
-    # Add more URLs as needed 
-] 
-
-@app.on_message(filters.command(["vcc", "a"]) & SUDOERS) 
+@app.on_message(filters.command(["vc", "a"]) & SUDOERS) 
 async def active_vc_command_handler(client, message): 
     active_chats_message = await generate_active_chats_message() 
     await message.reply(active_chats_message) 
@@ -25,11 +20,10 @@ async def active_vc_command_handler(client, message):
 async def generate_active_chats_message(): 
     active_chats_count = str(len(await get_active_chats())) 
     active_video_chats_count = str(len(await get_active_video_chats())) 
+    served_chats_count = str(len(await get_served_chats()))
+    served_users_count = str(len(await get_served_users()))
 
     total_chats_count = str(int(active_chats_count) + int(active_video_chats_count)) 
-
-    random_photo_url = random.choice(random_photos) 
-    random_photo_caption = "ᴄᴜʀʀᴇɴᴛ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ & ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ʟɪꜱᴛ🤗" 
 
     message = ( 
         "𝗕𝗼𝘁 𝗔𝗰𝘁𝗶𝘃𝗲 𝗖𝗵𝗮𝘁𝘀 𝗜𝗻𝗳𝗼 • 🔊\n" 
@@ -38,7 +32,11 @@ async def generate_active_chats_message():
         "•───────•\n" 
         f"🎥 ᴠɪᴅᴇᴏ 🎥 » {active_video_chats_count} Active\n" 
         "•──────•\n" 
-        f"💬 ᴛᴏᴛᴀʟ 💬 » {total_chats_count} ᴄʜᴀᴛs\n" 
-        "•──────•\n" 
+        f"💬 ᴛᴏᴛᴀʟ 💬 » {total_chats_count} Chats\n" 
+        "•──────•\n"
+        f"📊 𝙎𝙚𝙧𝙫𝙚𝙙 𝘾𝙝𝙖𝙩𝙨 📊\n» Chats » {served_chats_count}\n"
+        "•──────•\n"
+        f"👥 𝙎𝙚𝙧𝙫𝙚𝙙 𝙐𝙨𝙚𝙧𝙨 👥\n» Users » {served_users_count}\n"
+        "•──────•\n"
     ) 
     return message
